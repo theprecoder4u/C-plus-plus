db0b64d5c777126f56b6ea00c6f0446cb962ea6e
Preetam Patel <preetampatel620@gmail.com>
2025-09-27 22:58:38 +0530

    array indexing

---
+++ seven.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int chaiserve[5] = {85, 65, 67, 98, 74};
+    cout<<"Chai Served on day one: "<<chaiserve[0]<<endl;
+    cout<<"Chai Served on day two: "<<chaiserve[1]<<endl;
+    cout<<"Chai Served on day three: "<<chaiserve[2]<<endl;
+    cout<<"Chai Served on day four: "<<chaiserve[3]<<endl;
+    return 0;
+}
