bf03c07b01198155edf394fbd20604c8c6f89f10
Preetam Patel <preetampatel620@gmail.com>
2025-09-27 22:58:08 +0530

    array initialization

---
+++ six.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int chaitemp[5] = {85, 65, 67, 98, 74};
+    cout<<"Chai Temperature: ";
+    for(int i=0; i<5; i++){
+        cout<<chaitemp[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
